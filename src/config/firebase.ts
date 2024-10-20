b422bdb1c471a25d1cb200c5be39eac28dc3c73c
Abr009 <bradithya945@gmail.com>
2024-10-20 10:52:51 +0530

    Initial commit

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDotscF9kHO9XdfOWwH00MMrlbMvGhnfEg",
-  authDomain: "pedro-react-15aa8.firebaseapp.com",
-  projectId: "pedro-react-15aa8",
-  storageBucket: "pedro-react-15aa8.appspot.com",
-  messagingSenderId: "183528159061",
-  appId: "1:183528159061:web:d987a4f61c047e538f61dc"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
